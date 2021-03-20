c80f664d7e0e5a665eb4893db4e52e55b5a56b78
joshah98 <josha.heimann@alumni.ubc.ca>
2021-03-20 02:22:27 -0700

    Added floorplan, switched to ejs

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const Datastore = require('nedb');
+const path = require('path');
 
 // Start listening to port 8000 for updates from tracker
 const app = express();
 app.listen(8000, () => console.log('listening to 8000...'));
+
+// Set all the pie chart variables
+let t1off = 0; // These are all ventilators of type 1 that have the off status
+let t1on = 0;
+let t2off = 0;
+let t2on = 0;
 
 // Create database to store tracker info
 const db = new Datastore('vents.db');
@@ -11,31 +18,32 @@ db.loadDatabase();
 
 // This decides what is public to the client side, in this case just index.html
 app.use(express.static('public'));
-app.use(express.json())
+app.get('/', (req, res) => {
+    console.log('rendering');
+    res.render('index', {t1off: t1off, t1on: t1on, t2off: t2off, t2on: t2on});
+});
+app.set('view engine', 'ejs');
+app.use(express.json());
 
 // When the tracker posts, insert it into the database
-app.post('/', (req, res) => {
-    // console.log(req.body);
-    
+app.post('/', (req, res) => {    
     db.update({"_id": req.body._id}, { $set: {"location": req.body.location, "status": req.body.status}}, {}, (err, numReplaced) => {
         if (numReplaced === 0) {
             db.insert(req.body);
         }
     });
-
+    
     // Verifying that there are no duplicates in the db (weird bug shows duplicates when updating)
     db.find({"_id": req.body._id}, (err, docs) => {
         console.log(docs.length);
     });
-
+    
     res.end();
 });
 
-// Set all the pie chart variables
-let t1off; // These are all ventilators of type 1 that have the off status
-let t1on;
-let t2off;
-let t2on;
+
+
+
 
 const interval = setInterval(function() {
     
@@ -56,6 +64,8 @@ const interval = setInterval(function() {
     console.log("t1on: " +t1on);
     console.log("t2off: " +t2off);
     console.log("t2on: " +t2on);  
-    console.log("-----------------------------------------------") 
-}, 5000);
+    console.log("-----------------------------------------------") ;
+ 
+    
+}, 3000);
  
